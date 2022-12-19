c0a336ef2918de558ac18486aa76261cac5a3249
Fabian Enrique Pedreros Camargo <32172901+FabianPedreros@users.noreply.github.com>
2022-12-18 22:37:52 -0500

    Offensive

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ offensive_data[offensive_data.columns] = offensive_data.apply(lambda x: x.str.st
 offensive_data["Passes"]=offensive_data["Passes"].replace(",", "", regex=True)
 offensive_data["Pass Completions"]=offensive_data["Pass Completions"].replace(",", "", regex=True)
 
-offensive_data.iloc[:,1:] = offensive_data.iloc[:,1:].astype(int)
+offensive_data.iloc[:,1:] = offensive_data.iloc[:,1:].astype('int64')
 
 
 # Scrap defensive data
@@ -173,7 +173,7 @@ for row in soup4.find('tbody').find_all('tr'):
 
 goalkeeping_data[goalkeeping_data.columns] = goalkeeping_data.apply(lambda x: x.str.strip())
 
-goalkeeping_data.iloc[:,1:] = goalkeeping_data.iloc[:,1:].astype(int)
+goalkeeping_data.iloc[:,1:] = goalkeeping_data.iloc[:,1:].astype('int64')
 
 
 # Streamlit
@@ -185,8 +185,8 @@ with open('style.css') as f:
     
 st.sidebar.header('Dashboard FIFA World Cup Qatar 2022')
 
-st.sidebar.subheader('Medidas')
-medida = st.sidebar.selectbox('Medidas a comparar por equipo:', offensive_data.columns) 
+st.sidebar.subheader('Datos ofensivos')
+medida = st.sidebar.selectbox('Medidas a comparar por equipo:', offensive_data.columns[1:]) 
 
 st.sidebar.subheader('Donut chart parameter')
 donut_theta = st.sidebar.selectbox('Select data', ('q2', 'q3'))
@@ -202,44 +202,55 @@ Created with ❤️ by [Data Professor](https://youtube.com/dataprofessor/).
 
 
 # Row A
-st.markdown('### FIFA World Cup Qatar 2022')
-a1, a2, a3 = st.columns(3)
-a2.image(Image.open('logo.jpg'))
+st.markdown('## FIFA World Cup Qatar 2022')
+st.image(Image.open('logo.jpg'))
+
+
+#a1, a2, a3 = st.columns(3)
+#a2.image(Image.open('logo.jpg'))
 
 
 # Row B
-seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
-stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
+#seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
+#stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
 
-c1, c2 = st.columns((7,3))
-with c1:
-    st.markdown('### Comparación de selecciones')
-    plost.bar_chart(
-    data=offensive_data,
-    bar=offensive_data['Passes'],
-    value='National selection')
-with c2:
-    st.markdown('### Donut chart')
-    plost.donut_chart(
-        data=stocks,
-        theta=donut_theta,
-        color='company',
-        legend='bottom', 
-        use_container_width=True)
+#c1, c2 = st.columns((7,3))
+#with c1:
+#    st.markdown('### Comparación de selecciones')
+#    plost.bar_chart(
+#    data=offensive_data,
+#    bar=offensive_data['Passes'],
+#    value='National selection')
+#with c2:
+#    st.markdown('### Donut chart')
+#    plost.donut_chart(
+#        data=stocks,
+#        theta=donut_theta,
+#        color='company',
+#        legend='bottom', 
+#        use_container_width=True)
 
 # Row C
-st.markdown('### Line chart')
-st.line_chart(seattle_weather, x = 'date', y = plot_data, height = plot_height)
+#st.markdown('### Line chart')
+#st.line_chart(seattle_weather, x = 'date', y = plot_data, height = plot_height)
 
-st.dataframe(offensive_data)
+st.markdown('### Datos ofensivos')
 
-pie_chart = px.pie(offensive_data,
-                    title = 'Pases generados',
-                    values = 'Passes',
-                    names = 'National selection')
+import seaborn as sns
 
-st.plotly_chart(pie_chart)
+cm = sns.light_palette("red", as_cmap = True)
 
+
+st.dataframe(offensive_data.style.background_gradient(cmap=cm))
+
+#pie_chart = px.pie(offensive_data,
+#                    title = 'Pases generados',
+#                    values = 'Passes',
+#                    names = 'National selection')
+
+#st.plotly_chart(pie_chart)
+
+# Bar chart offensive data
 bar_chart = px.bar(offensive_data,
                     x = 'National selection',
                     y = medida,
@@ -253,3 +264,123 @@ bar_chart = px.bar(offensive_data,
 bar_chart.update_layout(barmode='stack', xaxis={'categoryorder': 'total descending'})
 
 st.plotly_chart(bar_chart)
+
+
+# transform offensive data por partido
+offensive_data_pp = offensive_data.copy()
+offensive_data_pp.iloc[:,1:] = offensive_data_pp.iloc[:,1:].astype(int)
+
+# offensive_data_pp
+
+#offensive_data_pp.iloc[:,1:] = offensive_data_pp.iloc[:,1:].div(pd.Series([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], index = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]), axis = 'index')
+
+offensive_data_pp.iloc[:,1:] = offensive_data_pp.iloc[:,1:].div(pd.Series(offensive_data['Games Played'], index = offensive_data_pp.index), axis = 'index')
+
+offensive_data_pp['Possession Time Minutes Avg'] = offensive_data['Possession Time Minutes Avg']
+
+offensive_data_pp=offensive_data_pp.round(1)
+
+
+# Show offensive avg data table
+st.markdown('**Datos promedio**')
+st.dataframe(offensive_data_pp.style.background_gradient(cmap=cm))
+
+# Bar chart offensive data por partido
+bar_chart_o_pp = px.bar(offensive_data_pp,
+                    x = 'National selection',
+                    y = medida,
+                    text= medida,
+                    template = 'plotly_white',
+                    color = 'National selection',
+                    width = 1000,
+                    height= 600,
+                    title = f'Offensive variable (Avg): {medida}')
+
+bar_chart_o_pp.update_layout(barmode='stack', xaxis={'categoryorder': 'total descending'})
+
+st.plotly_chart(bar_chart_o_pp)
+
+# Scatter for related measures
+
+c1, c2 = st.columns(2)
+
+with c1:
+    medida_o_pp_1 = st.selectbox('Medidas a comparar por equipo:', ['Shots', 'Crosses', 'Passes','Corner Kicks', 'Offsides']) 
+
+with c2:
+    medida_o_pp_2 = st.selectbox('Medidas a comparar por equipo:', ['Shots On Goal', 'Shots On Post', 'Shots Off Target', 'Shot Attempts Blocked','Possession Time Minutes Avg', 'Crosses Successful', 'Pass Completions', 'Pass Attempts - Long','Pass Completions - Long'])
+
+scatter_o_pp = px.scatter(offensive_data_pp,
+                    x = medida_o_pp_1,
+                    y = medida_o_pp_2,
+                    text= 'National selection',
+                    template = 'plotly_white',
+                    color = 'National selection',
+                    width = 1000,
+                    height= 600,
+                    title = f'Offensive variable (Avg):',
+                    trendline='ols')
+
+scatter_o_pp.update_traces(marker={'size': 20})
+
+st.plotly_chart(scatter_o_pp)
+
+offensive_data_pp_t=offensive_data_pp.transpose()
+offensive_data_pp_t=offensive_data_pp_t.reset_index()
+offensive_data_pp_t.columns = offensive_data_pp_t.iloc[0]
+offensive_data_pp_t= offensive_data_pp_t.drop(offensive_data_pp_t.index[0])
+#offensive_data_pp_t
+
+st.markdown('#### Comparación entre selecciones (Avg)')
+
+# Variables for butterfly plot
+
+d1, d2 = st.columns(2)
+
+with d1:
+    medida_o_pp_b1 = st.selectbox('Selección:', offensive_data_pp_t.columns[1:].sort_values(ascending = False))
+
+with d2:
+    medida_o_pp_b2 = st.selectbox('Selección:', offensive_data_pp_t.columns[1:].sort_values(ascending = True))
+
+
+#Butterfly plot offensive data
+
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+fig = make_subplots(rows=1, cols=2, specs=[[{}, {}]], shared_xaxes=False,
+                    shared_yaxes=True, horizontal_spacing=0)
+
+fig.append_trace(go.Bar(x=offensive_data_pp_t[medida_o_pp_b1],
+                     y=offensive_data_pp_t.iloc[:,0], 
+                     text=offensive_data_pp_t[medida_o_pp_b1].map('{:,.1f}'.format), #Display the numbers with thousands separators in hover-over tooltip 
+                     textposition='inside',
+                     orientation='h', 
+                     width=0.7, 
+                     showlegend=False, 
+                     marker_color='#686cfc'), 
+                     1, 1) # 1,1 represents row 1 column 1 in the plot grid
+
+
+fig.append_trace(go.Bar(x=offensive_data_pp_t[medida_o_pp_b2],
+                     y=offensive_data_pp_t.iloc[:,0], 
+                     text=offensive_data_pp_t[medida_o_pp_b2].map('{:,.1f}'.format),
+                     textposition='inside',
+                     orientation='h', 
+                     width=0.7, 
+                     showlegend=False, 
+                     marker_color='#f0543c'), 
+                     1, 2) # 1,2 represents row 1 column 2 in the plot grid
+
+fig.update_xaxes(showticklabels=False,title_text=medida_o_pp_b1, row=1, col=1, range=[100,0])
+fig.update_xaxes(showticklabels=False,title_text=medida_o_pp_b2, row=1, col=2, range=[0,100])
+
+fig.update_layout(title_text="Comparación entre selecciones", 
+                  width=800, 
+                  height=700,
+                  title_x=0.9,
+                  xaxis1={'side': 'top'},
+                  xaxis2={'side': 'top'},)
+
+st.plotly_chart(fig)
